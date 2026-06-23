5122d57bd0e8e57512d09656a4604bdfab88c871
Rittika27 <rittika27@example.com>
2026-06-23 15:20:30 +0530

    Remove old flat-structure folders, keep only Week1 structured layout

--- EcommerceSearch/Product.cs
+++
@@ -1,6 +0,0 @@
-public class Product
-{
-    public int ProductId { get; set; }
-    public string ProductName { get; set; } = string.Empty;
-    public string Category { get; set; } = string.Empty;
-}
--- SingletonPatternExample/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Logger logger1 = Logger.GetInstance();
-Logger logger2 = Logger.GetInstance();
-
-logger1.Log("Singleton Pattern Working");
-
-Console.WriteLine(Object.ReferenceEquals(logger1, logger2));
